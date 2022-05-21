45ef236ac1b43ed84e3aed2630b1df03873bfa8a
Henry Escobar <henrye@gmail.com>
2022-05-21 15:27:22 -0700

    commit tree ba4a5c9c031e4b7cbb77f3d9ee3dba9e1c948520

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Henry';
+
+function upperCaseCustomerName(name){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+bestCustomer = 'not bob';
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob';
+    return leastFavoriteCustomer;
+}
